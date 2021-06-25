c17a44661e9d445340b3741591eec66a32a8c497
Alexander Petrov <alexander.petrov@nav.no>
2021-06-25 15:00:35 +0200

    Add vault

--- sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/SendsoknadApplication.java
+++ sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/SendsoknadApplication.java
@@ -2,8 +2,14 @@ package no.nav.sbl.sendsoknad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import no.nav.sbl.dialogarena.config.SikkerhetsConfig;
+import no.nav.sbl.dialogarena.config.SoknadinnsendingConfig;
+
 
 @SpringBootApplication
+//@Import(value = {SoknadinnsendingConfig.class,SikkerhetsConfig.class})
 public class SendsoknadApplication {
 
 	public static void main(String[] args) {
