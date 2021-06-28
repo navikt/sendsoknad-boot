8a4a014e347bebde8f7c75c69839010fe9a79906
Alexander Petrov <alexander.petrov@nav.no>
2021-06-28 15:03:16 +0200

    change env names

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 echo "Leser secrets fra disk til environment"
 
+export TEST_EXPORT=hellooo
 
 if test -f "/secrets/serviceuser/username"; then
   export SERVICEUSER_USERNAME=$(cat /secrets/serviceuser/username)
@@ -7,16 +8,16 @@ if test -f "/secrets/serviceuser/username"; then
 fi
 
 if test -f "/secrets/sendsoknad-q1/jdbc_url"; then
-  export SERVICEUSER_USERNAME=$(cat /secrets/sendsoknad-q1/jdbc_url)
-  echo "Eksporterer variabel SERVICEUSER_USERNAME"
+  export DATASOURCE_URL=$(cat /secrets/sendsoknad-q1/jdbc_url)
+  echo "Eksporterer variabel DATASOURCE_URL"
 fi
 
 if test -f "/secrets/sendsoknad-q1/username"; then
-  export SERVICEUSER_USERNAME=$(cat /secrets/sendsoknad-q1/username)
-  echo "Eksporterer variabel ORACLE_USERNAME"
+  export DATASOURCE_USERNAME=$(cat /secrets/sendsoknad-q1/username)
+  echo "Eksporterer variabel DATASOURCE_USERNAME"
 fi
 
 if test -f "/secrets/sendsoknad-q1/password"; then
-  export SERVICEUSER_USERNAME=$(cat /secrets/sendsoknad-q1/password)
-  echo "Eksporterer variabel ORACLE_PASSWORD"
+  export DATASOURCE_PASSWORD=$(cat /secrets/sendsoknad-q1/password)
+  echo "Eksporterer variabel DATASOURCE_PASSWORD"
 fi
