0521b06bb43ea1301a7ca22b59e98c4523a0e3a3
Alexander Petrov <alexander.petrov@nav.no>
2021-06-29 10:35:31 +0200

    change mount paths

--- init.sh
+++ init.sh
@@ -2,22 +2,24 @@ echo "Leser secrets fra disk til environment"
 
 export TEST_EXPORT=hellooo
 
+echo $TEST_EXPORT
+
 if test -f "/secrets/serviceuser/username"; then
   export SERVICEUSER_USERNAME=$(cat /secrets/serviceuser/username)
   echo "Eksporterer variabel SERVICEUSER_USERNAME"
 fi
 
-if test -f "/secrets/sendsoknad-q1/jdbc_url"; then
-  export DATASOURCE_URL=$(cat /secrets/sendsoknad-q1/jdbc_url)
+if test -f "/secrets/oracle-q1/config/jdbc_url"; then
+  export DATASOURCE_URL=$(cat /secrets/oracle-q1/config/jdbc_url)
   echo "Eksporterer variabel DATASOURCE_URL"
 fi
 
-if test -f "/secrets/sendsoknad-q1/username"; then
-  export DATASOURCE_USERNAME=$(cat /secrets/sendsoknad-q1/username)
+if test -f "/secrets/oracle-q1/user/username"; then
+  export DATASOURCE_USERNAME=$(cat /secrets/oracle-q1/user/username)
   echo "Eksporterer variabel DATASOURCE_USERNAME"
 fi
 
-if test -f "/secrets/sendsoknad-q1/password"; then
-  export DATASOURCE_PASSWORD=$(cat /secrets/sendsoknad-q1/password)
+if test -f "/secrets/oracle-q1/user/password"; then
+  export DATASOURCE_PASSWORD=$(cat /secrets/oracle-q1/user/password)
   echo "Eksporterer variabel DATASOURCE_PASSWORD"
 fi
