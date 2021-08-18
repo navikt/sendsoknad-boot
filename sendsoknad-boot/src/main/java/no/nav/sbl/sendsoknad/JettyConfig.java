46d5a6cb6de81b6032f7031582ecd4e5f8dcbf1d
Alexander Petrov <alexander.petrov@nav.no>
2021-08-18 15:30:12 +0200

    test JAAS

--- sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/JettyConfig.java
+++ sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/JettyConfig.java
@@ -1,9 +1,14 @@
 package no.nav.sbl.sendsoknad;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import org.eclipse.jetty.jaas.JAASLoginService;
+import org.eclipse.jetty.security.ConstraintMapping;
 import org.eclipse.jetty.security.ConstraintSecurityHandler;
 import org.eclipse.jetty.security.LoginService;
+import org.eclipse.jetty.util.security.Constraint;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.web.embedded.JettyWebServerFactoryCustomizer;
 import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
@@ -11,8 +16,11 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import no.nav.sbl.dialogarena.soknadinnsending.business.service.FaktaService;
+
 @Configuration
 public class JettyConfig {
+	private static final Logger logger = getLogger(JettyConfig.class);
 
 	
 	@Bean
@@ -20,26 +28,39 @@ public class JettyConfig {
 	
         return container -> {
             if (container instanceof JettyServletWebServerFactory) {
+            	logger.info("Adding jetty server customizer");
                 ((JettyServletWebServerFactory) container).addServerCustomizers(jettyServerCustomizer);
             }
         };
     }
 
     @Bean
-    JettyServerCustomizer jettyServerCustomizer(final LoginService loginService) {
-        return server -> ((WebAppContext) server.getHandler()).getSecurityHandler().setLoginService(loginService);// .setSecurityHandler(constraintSecurityHandler);
+    JettyServerCustomizer jettyServerCustomizer(final LoginService loginService,ConstraintSecurityHandler constraintSecurityHandler) {
+        return server -> {
+        	logger.info("Setting loginService");
+        	((WebAppContext) server.getHandler()).setSecurityHandler(constraintSecurityHandler);
+       // 	((WebAppContext) server.getHandler()).getSecurityHandler().setLoginService(loginService); 
+        };// .setSecurityHandler(constraintSecurityHandler);
     }
-/*
+
     @Bean
     ConstraintSecurityHandler constraintSecurityHandler(final LoginService loginService) {
         final ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
 
         securityHandler.setLoginService(loginService);
-       // getConstraintMappings().forEach(securityHandler::addConstraintMapping);
+        
+       Constraint constraint = new Constraint();
+       constraint.setAuthenticate(true);
+       ConstraintMapping mapping = new ConstraintMapping();
+       mapping.setPathSpec("/*");
+       mapping.setConstraint(constraint);
+       securityHandler.addConstraintMapping(mapping);
+       securityHandler.setLoginService(loginService);
+        // getConstraintMappings().forEach(securityHandler::addConstraintMapping);
 
         return securityHandler;
     }
-    */
+   
     
     @Bean
     LoginService loginService()  {
