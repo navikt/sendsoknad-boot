037eae5a4d12b6299a5c6da99c75cedfa4f1e726
Alexander Petrov <alexander.petrov@nav.no>
2021-08-19 10:17:05 +0200

    logging

--- sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/SendsoknadApplication.java
+++ sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/SendsoknadApplication.java
@@ -1,5 +1,8 @@
 package no.nav.sbl.sendsoknad;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration;
@@ -17,9 +20,13 @@ import no.nav.sbl.dialogarena.security.ThreadLocalSubjectHandler;
 @EnableWebMvc
 @Import(value = {JettyConfig.class,SoknadinnsendingConfig.class,SikkerhetsConfig.class})
 public class SendsoknadApplication {
+	private static final Logger logger = getLogger(SendsoknadApplication.class);
+
 
 	public static void main(String[] args) {
+
 		final String loginConfFile = SendsoknadApplication.class.getClassLoader().getResource("login.conf").getFile();
+		logger.info("login.conf file location is " + loginConfFile);
 		System.setProperty("java.security.auth.login.config", loginConfFile);
 	    System.setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", ThreadLocalSubjectHandler.class.getName());
 	    SpringApplication.run(SendsoknadApplication.class, args);
