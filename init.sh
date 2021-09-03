4bc7ce4e9f582ef825b62b3135e962b926d7863f
Alexander Petrov <alexander.petrov@nav.no>
2021-09-03 14:46:20 +0200

    remove q1 dependencies

--- init.sh
+++ init.sh
@@ -1,9 +1,5 @@
 echo "Leser secrets fra disk til environment"
 
-export TEST_EXPORT=hellooo
-
-echo $TEST_EXPORT
-
 if test -f "/secrets/serviceuser/username"; then
   export SERVICEUSER_USERNAME=$(cat /secrets/serviceuser/username)
   echo "Eksporterer variabel SERVICEUSER_USERNAME"
@@ -24,18 +20,18 @@ if test -f "/secrets/personinformasjon/password"; then
   echo "Eksporterer variabel PERSONINFORMASJON_PASSWORD"
 fi
 
-if test -f "/secrets/oracle-q1/config/jdbc_url"; then
-  export DATASOURCE_URL=$(cat /secrets/oracle-q1/config/jdbc_url)
+if test -f "/secrets/oracle/config/jdbc_url"; then
+  export DATASOURCE_URL=$(cat /secrets/oracle/config/jdbc_url)
   echo "Eksporterer variabel DATASOURCE_URL"
 fi
 
-if test -f "/secrets/oracle-q1/user/username"; then
-  export DATASOURCE_USERNAME=$(cat /secrets/oracle-q1/user/username)
+if test -f "/secrets/oracle/user/username"; then
+  export DATASOURCE_USERNAME=$(cat /secrets/oracle/user/username)
   echo "Eksporterer variabel DATASOURCE_USERNAME"
 fi
 
-if test -f "/secrets/oracle-q1/user/password"; then
-  export DATASOURCE_PASSWORD=$(cat /secrets/oracle-q1/user/password)
+if test -f "/secrets/oracle/user/password"; then
+  export DATASOURCE_PASSWORD=$(cat /secrets/oracle/user/password)
   echo "Eksporterer variabel DATASOURCE_PASSWORD"
 fi
 
