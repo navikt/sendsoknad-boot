2bb751c3219619c8f02927f01759225fbc253c92
Alexander Petrov <alexander.petrov@nav.no>
2021-09-30 13:25:47 +0200

    use aspect for MDC put behandlingsId

--- sendsoknad-auth/src/main/java/no/nav/modig/presentation/logging/session/MDCFilter.java
+++ sendsoknad-auth/src/main/java/no/nav/modig/presentation/logging/session/MDCFilter.java
@@ -1,8 +1,6 @@
 package no.nav.modig.presentation.logging.session;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,11 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 import no.nav.modig.common.MDCOperations;
 import no.nav.modig.core.context.SubjectHandler;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.HandlerMapping;
 
 /**
  * Se <a href=http://confluence.adeo.no/display/Modernisering/MDCFilter>Utviklerhåndbok - Logging - Sporingslogging -
@@ -38,43 +34,20 @@ public class MDCFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         log.debug("Entering filter to extract values and put on MDC for logging");
 
-        Map<String, String> pathMap = (Map<String,String>)httpServletRequest.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);  
-        
-        String behandlingsId= (pathMap!=null && pathMap.containsKey("behandlingsId")) ? pathMap.get("behandlingsId") : "";
-        
-       // if (!StringUtils.isEmpty(behandlingsId)) {
-        	
-        //	subjectHandler.setBehandlingsId(behandlingsId);
-       // }
-       // else {
-        	
-       // }
-        log.info("Behandlings id extracted from " + httpServletRequest.getRequestURL() + " behandlingsId is " + behandlingsId);
-        
-        if (StringUtils.isEmpty(behandlingsId)) {
-        	behandlingsId = (String)httpServletRequest.getSession().getAttribute("behandlingsId");
-        	log.info("Behandlings id extracted from session behandlingsId is " + behandlingsId);
-        }
-        
+      
         String consumerId = subjectHandler.getConsumerId() != null ? subjectHandler.getConsumerId() : "";
-       // behandlingsId = subjectHandler.getBehandlingsId() != null ? subjectHandler.getBehandlingsId() : "";
         String callId = MDCOperations.generateCallId();
 
         MDCOperations.putToMDC(MDCOperations.MDC_CALL_ID, callId);
         MDCOperations.putToMDC(MDCOperations.MDC_CONSUMER_ID, consumerId);
-        MDCOperations.putToMDC(MDCOperations.MDC_BEHANDLINGS_ID, behandlingsId);
         log.debug("Values added");
 
         try {
             filterChain.doFilter(httpServletRequest, httpServletResponse);
         } finally {
-        	String bId = MDCOperations.getFromMDC(MDCOperations.MDC_BEHANDLINGS_ID);
-        	httpServletRequest.getSession().setAttribute("behandlingsId", bId);
-        	log.info("setting behandlingsid in session " + bId);
             MDCOperations.remove(MDCOperations.MDC_CALL_ID);
             MDCOperations.remove(MDCOperations.MDC_CONSUMER_ID);
             MDCOperations.remove(MDCOperations.MDC_BEHANDLINGS_ID);
-            
             log.debug("Cleared MDC session");
         }
     }
