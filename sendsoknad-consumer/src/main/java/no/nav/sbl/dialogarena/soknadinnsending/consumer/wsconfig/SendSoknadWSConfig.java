5b4f845e23a7ceb4ff845a035df85b6dc2eee05f
Alexander Petrov <alexander.petrov@nav.no>
2021-10-20 12:16:04 +0200

    enable user security

--- sendsoknad-consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/SendSoknadWSConfig.java
+++ sendsoknad-consumer/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/wsconfig/SendSoknadWSConfig.java
@@ -39,7 +39,7 @@ public class SendSoknadWSConfig {
     @Bean
     public SendSoknadPortType sendSoknadEndpoint() {
     	//@TODO dette var usersecurity
-    	SendSoknadPortType port = factory().withSystemSecurity().get();
+    	SendSoknadPortType port = factory().withUserSecurity().get();
     	return (SendSoknadPortType) Proxy.newProxyInstance(BehandlingsIdSubjectDecorator.class.getClassLoader(),new Class<?> [] { SendSoknadPortType.class }, new BehandlingsIdSubjectDecorator(port));
           
     }
