15541433afa2fb6b6121e142eab65ead4d3908a1
Alexander Petrov <alexander.petrov@nav.no>
2021-11-11 19:43:59 +0100

    test

--- sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/SendsoknadApplication.java
+++ sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/SendsoknadApplication.java
@@ -4,6 +4,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.prometheus.PrometheusMetricsExportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
@@ -18,7 +19,7 @@ import no.nav.sbl.dialogarena.security.ThreadLocalSubjectHandler;
 @SpringBootApplication(exclude = {HttpEncodingAutoConfiguration.class})
 @EnableCaching
 @EnableWebMvc
-@Import(value = {JettyConfig.class,SoknadinnsendingConfig.class,SikkerhetsConfig.class,FlywayConfig.class})
+@Import(value = {JettyConfig.class,SoknadinnsendingConfig.class,SikkerhetsConfig.class,FlywayConfig.class,PrometheusMetricsExportAutoConfiguration.class})
 public class SendsoknadApplication {
 	private static final Logger logger = getLogger(SendsoknadApplication.class);
 
