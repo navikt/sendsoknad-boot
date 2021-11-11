1d08ffe9aee89a072ffe262bc751069d39623803
Alexander Petrov <alexander.petrov@nav.no>
2021-11-11 20:45:28 +0100

    fix prometheus

--- sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/SendsoknadApplication.java
+++ sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/SendsoknadApplication.java
@@ -19,7 +19,7 @@ import no.nav.sbl.dialogarena.security.ThreadLocalSubjectHandler;
 @SpringBootApplication(exclude = {HttpEncodingAutoConfiguration.class})
 @EnableCaching
 @EnableWebMvc
-@Import(value = {JettyConfig.class,SoknadinnsendingConfig.class,SikkerhetsConfig.class,FlywayConfig.class,PrometheusMetricsExportAutoConfiguration.class})
+@Import(value = {JettyConfig.class,SoknadinnsendingConfig.class,SikkerhetsConfig.class,FlywayConfig.class})
 public class SendsoknadApplication {
 	private static final Logger logger = getLogger(SendsoknadApplication.class);
 
