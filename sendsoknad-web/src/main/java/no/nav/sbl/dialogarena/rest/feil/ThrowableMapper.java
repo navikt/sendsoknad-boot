084b149c7223741f512218c6333d832d838502d5
Alexander Petrov <alexander.petrov@nav.no>
2021-11-23 10:48:48 +0100

    revert throwable mapper

--- sendsoknad-web/src/main/java/no/nav/sbl/dialogarena/rest/feil/ThrowableMapper.java
+++ sendsoknad-web/src/main/java/no/nav/sbl/dialogarena/rest/feil/ThrowableMapper.java
@@ -29,14 +29,7 @@ public class ThrowableMapper implements ExceptionMapper<Throwable> {
             } else {
                 logger.error(e.getMessage(), e);
             }
-            
-            
-            if (exception.getResponse().getStatus() == 404) {
-                return Response.status(Status.NOT_FOUND).build();
-            }
-            else {
-                return status(exception.getResponse().getStatus()).type(APPLICATION_JSON).entity(new Feilmelding("web_application_error", "Noe uventet feilet")).build();
-            }
+            return status(exception.getResponse().getStatus()).type(APPLICATION_JSON).entity(new Feilmelding("web_application_error", "Noe uventet feilet")).build();
         } else {
             logger.error("Noe uventet feilet", e);
             return serverError().header(NO_BIGIP_5XX_REDIRECT, true).type(APPLICATION_JSON).entity(new Feilmelding("unexpected_error", "Noe uventet feilet")).build();
