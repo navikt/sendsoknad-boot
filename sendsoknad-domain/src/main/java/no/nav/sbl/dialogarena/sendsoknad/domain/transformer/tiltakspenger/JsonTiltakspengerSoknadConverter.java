317f5527a722b7fe1b5ee7b4c156c2124f277cb4
Sneha Desai <sneha.desai@nav.no>
2021-11-25 12:39:00 +0100

    La json konverter til tiltakspenger #40 Co-authored-by: sneha-d-desai <sneha.desai@nav.no> Co-authored-by: pjwalstrom <Per.Jorgen.Walstrom@nav.no>

---
+++ sendsoknad-domain/src/main/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/tiltakspenger/JsonTiltakspengerSoknadConverter.java
@@ -0,0 +1,73 @@
+package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.tiltakspenger;
+
+import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class JsonTiltakspengerSoknadConverter {
+    private JsonTiltakspengerSoknadConverter() {
+
+    }
+
+    static JsonTiltakspengerSoknad tilJsonSoknad(WebSoknad webSoknad) {
+        return new JsonTiltakspengerSoknad()
+                .medSoknadId(webSoknad.getSoknadId())
+                .medSkjemaNummer(webSoknad.getskjemaNummer())
+                .medVersjon(webSoknad.getVersjon())
+                .medUuid(webSoknad.getUuid())
+                .medBrukerBehandlingId(webSoknad.getBrukerBehandlingId())
+                .medBehandlingskjedeId(webSoknad.getBehandlingskjedeId())
+                .medFakta(tilJsonFakta(webSoknad))
+                .medStatus(webSoknad.getStatus())
+                .medAktoerId(webSoknad.getAktoerId())
+                .medOpprettetDato(webSoknad.getOpprettetDato())
+                .medSistLagret(webSoknad.getSistLagret())
+                .medDelstegStatus(webSoknad.getDelstegStatus())
+                .medJournalforendeEnhet(webSoknad.getJournalforendeEnhet())
+                .medSoknadPrefix(webSoknad.getSoknadPrefix())
+                .medSoknadUrl(webSoknad.getSoknadUrl())
+                .medFortsettSoknadUrl(webSoknad.getFortsettSoknadUrl())
+                .medVedlegg(tilJsonVedlegg(webSoknad))
+                .medErEttersending(webSoknad.erEttersending());
+    }
+
+
+    private static List<JsonTiltakspengerFaktum> tilJsonFakta(WebSoknad webSoknad) {
+        return webSoknad.getFakta().stream().map(faktum -> new JsonTiltakspengerFaktum()
+                .medFaktumId(faktum.getFaktumId())
+                .medSoknadId(faktum.getSoknadId())
+                .medParentFaktum(faktum.getParrentFaktum())
+                .medFaktumKey(faktum.getKey())
+                .medFaktumValue(faktum.getValue())
+                .medFaktumEgenskaper(faktum.getFaktumEgenskaper())
+                .medFaktumProperties(faktum.getProperties())
+                .medFaktumType(faktum.getType())
+        ).collect(toList());
+    }
+
+    private static List<VedleggJson> tilJsonVedlegg(WebSoknad webSoknad) {
+        return webSoknad.getVedlegg()
+                .stream()
+                .map(vedlegg -> new VedleggJson()
+                        .medVedleggId(vedlegg.getVedleggId())
+                        .medSoknadId(vedlegg.getSoknadId())
+                        .medFaktumId(vedlegg.getFaktumId())
+                        .medSkjemaNummer(vedlegg.getSkjemaNummer())
+                        .medSkjemaNummerTillegg(vedlegg.getSkjemanummerTillegg())
+                        .medInnsendingsvalg(vedlegg.getInnsendingsvalg())
+                        .medOpprinneligInnsendingsvalg(vedlegg.getOpprinneligInnsendingsvalg())
+                        .medNavn(vedlegg.getNavn())
+                        .medStorrelse(vedlegg.getStorrelse())
+                        .medAntallSider(vedlegg.getAntallSider())
+                        .medOpprettetDato(vedlegg.getOpprettetDato())
+                        .medFillagerReferanse(vedlegg.getFillagerReferanse())
+                        .medUrls(vedlegg.getUrls())
+                        .medTittel(vedlegg.getTittel())
+                        .medAarsak(vedlegg.getAarsak())
+                        .medFilnavn(vedlegg.getFilnavn())
+                        .medMimetype(vedlegg.getMimetype()))
+                .collect(toList());
+    }
+}
--- sendsoknad-domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/tiltakspenger/TiltakspengerTilJsonTest.java
+++ sendsoknad-domain/src/test/java/no/nav/sbl/dialogarena/sendsoknad/domain/transformer/tiltakspenger/TiltakspengerTilJsonTest.java
@@ -1,15 +1,18 @@
 package no.nav.sbl.dialogarena.sendsoknad.domain.transformer.tiltakspenger;
 
-import no.nav.sbl.dialogarena.sendsoknad.domain.AlternativRepresentasjon;
-import no.nav.sbl.dialogarena.sendsoknad.domain.Faktum;
-import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
+import junit.framework.AssertionFailedError;
+import net.minidev.json.JSONObject;
+import no.nav.sbl.dialogarena.sendsoknad.domain.*;
 import org.junit.jupiter.api.Test;
 
+import static no.nav.sbl.dialogarena.sendsoknad.domain.SoknadInnsendingStatus.UNDER_ARBEID;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TiltakspengerTilJsonTest {
+
+    private JsonTiltakspengerSoknad jsonSoknad;
 
     @Test
     public void skalKonvertereTilAlternativRepresentasjon() {
@@ -27,19 +30,37 @@ class TiltakspengerTilJsonTest {
     }
 
     @Test
-    public void konvertererFaktumRett() {
-        Faktum faktum1 = new Faktum().medKey("informasjonsside.deltarIIntroprogram").medValue("false");
-        Faktum faktum2 = new Faktum().medKey("informasjonsside.institusjon").medValue("true");
-        Faktum faktum3 = new Faktum().medKey("informasjonsside.institusjon.ja.hvaslags").medValue("fengsel");
-        Faktum faktum4 = new Faktum().medKey("informasjonsside.kvalifiseringsprogram").medValue("false");
-        WebSoknad webSoknad = new WebSoknad().medFaktum(faktum1).medFaktum(faktum2).medFaktum(faktum3).medFaktum(faktum4);
+    public void skalKonverterefaktumStruktur() {
+        WebSoknad soknad = new WebSoknad()
+                .medId(1234)
+                .medskjemaNummer("NAV 76-13.45")
+                .medStatus(UNDER_ARBEID)
+                .medAktorId("12345")
+                .medDelstegStatus(DelstegStatus.VEDLEGG_VALIDERT)
+                .medFaktum(new Faktum().medKey("informasjonsside.deltarIIntroprogram").medValue("false"))
+                .medFaktum(new Faktum().medKey("informasjonsside.institusjon").medValue("true"))
+                .medFaktum(new Faktum().medKey("informasjonsside.institusjon.ja.hvaslags").medValue("fengsel"))
+                .medFaktum(new Faktum().medKey("informasjonsside.kvalifiseringsprogram").medValue("false"));
 
-        AlternativRepresentasjon result = new TiltakspengerTilJson().apply(webSoknad);
 
-        // TODO: konvertere til json fra content
-        //String json = result.getContent();
-        //TODO: hent verdien fra informasjonsside.institusjon
-        //assertTrue(json.);
-        
+        jsonSoknad = new TiltakspengerTilJson().transform(soknad);
+
+        assertThat(jsonSoknad.getSkjemaNummer()).isEqualTo("NAV 76-13.45");
+        assertThat(jsonSoknad.getAktoerId()).isEqualTo("12345");
+        assertThat(jsonSoknad.getStatus()).isEqualTo(UNDER_ARBEID);
+        assertThat(jsonSoknad.getFakta()).hasSize(4);
+        assertThat(getFaktum("informasjonsside.deltarIIntroprogram").getValue()).isEqualTo("false");
+        assertThat(getFaktum("informasjonsside.institusjon").getValue()).isEqualTo("true");
+        assertThat(getFaktum("informasjonsside.institusjon.ja.hvaslags").getValue()).isEqualTo("fengsel");
+        assertThat(getFaktum("informasjonsside.kvalifiseringsprogram").getValue()).isEqualTo("false");
+    }
+
+    private JsonTiltakspengerFaktum getFaktum(String key) {
+        return jsonSoknad
+                .getFakta()
+                .stream()
+                .filter(jsonTiltakspengerFaktum -> jsonTiltakspengerFaktum.getKey().equals(key))
+                .findFirst()
+                .orElseThrow(AssertionFailedError::new);
     }
 }
