181a08e15a95ee0d7100919370ef29c45e1aaa0d
Johan Sjöblom <johan.sjoblom@squeed.com>
2021-12-02 18:20:14 +0100

    Changing Hovedskjemadata to contain its type

--- sendsoknad-innsending/src/main/kotlin/no/nav/sbl/soknadinnsending/innsending/dto/Hovedskjemadata.kt
+++ sendsoknad-innsending/src/main/kotlin/no/nav/sbl/soknadinnsending/innsending/dto/Hovedskjemadata.kt
@@ -1,9 +1,5 @@
 package no.nav.sbl.soknadinnsending.innsending.dto
 
-data class Hovedskjemas(
-	val arkiv: Hovedskjemadata,
-	val fullversjon: Hovedskjemadata?,
-	val original: Hovedskjemadata?
-)
+data class Hovedskjemadata(val id: String, val fileSize: Long, val fileType: String, val type: HovedskjemaType)
 
-data class Hovedskjemadata(val id: String, val fileSize: Long, val fileType: String)
+enum class HovedskjemaType{ ARKIV, FULLVERSJON, ORIGINAL }
