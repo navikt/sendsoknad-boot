309f3f3bfaf2125244e87efb27995b380133a41d
Eirik <eirikmyklebost+github@gmail.com>
2022-02-04 14:09:29 +0100

    Created NavStsRestClient for System-user SAML

---
+++ sendsoknad-auth/src/main/java/no/nav/modig/security/sts/client/NavStsRestClient.java
@@ -0,0 +1,49 @@
+package no.nav.modig.security.sts.client;
+
+import no.nav.modig.core.context.ModigSecurityConstants;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Base64;
+import java.util.Objects;
+
+// See https://github.com/navikt/gandalf
+@Component
+public class NavStsRestClient {
+    private final WebClient webClient;
+    private final String authHeader;
+
+    public NavStsRestClient(
+            WebClient.Builder webClientBuilder,
+            @Value("${no.nav.modig.security.rest.url}") String stsUrl,
+            @Value("${" + ModigSecurityConstants.SYSTEMUSER_USERNAME + "}") String systemUser,
+            @Value("${" + ModigSecurityConstants.SYSTEMUSER_PASSWORD + "}") String systemPassword) {
+        this.webClient = webClientBuilder.baseUrl(stsUrl).build();
+        this.authHeader = Base64.getEncoder().encodeToString((systemUser + ":" + systemPassword).getBytes());
+    }
+
+    public String getSystemSaml() {
+        var response = this.webClient
+                .get()
+                .uri("/rest/v1/sts/samltoken")
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + authHeader)
+                .retrieve()
+                .bodyToMono(ResponseModel.class)
+                .block();
+
+        return Objects.requireNonNull(response).decodedToken();
+    }
+
+    private static class ResponseModel {
+        public String access_token;
+        public String issued_token_type;
+        public String token_type;
+        public int expires_in;
+
+        String decodedToken() {
+            return new String(Base64.getDecoder().decode(access_token));
+        }
+    }
+}
