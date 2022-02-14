474543ba761fe6da5419de1d077e7d7f6c1e79b8
Eirik <eirikmyklebost+github@gmail.com>
2022-02-14 12:52:19 +0100

    Update ServiceBuilder.java

--- sendsoknad-auth/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/ServiceBuilder.java
+++ sendsoknad-auth/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/consumer/ServiceBuilder.java
@@ -68,7 +68,7 @@ public final class ServiceBuilder<T> {
     }
 
     public ServiceBuilder<T> withLogging() {
-        factoryBean.getFeatures().add(new LoggingFeatureUtenBinaryOgUtenSamlTokenLogging());
+        factoryBean.getFeatures().add(new LoggingFeatureUtenBinaryOgUtenSamlTokenLogging(false));
         return this;
     }
 
