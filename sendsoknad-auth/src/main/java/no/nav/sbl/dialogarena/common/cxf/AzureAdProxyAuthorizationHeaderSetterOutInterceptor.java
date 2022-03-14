fb110b5a7d62deb4e38afbfe291fc1381595c8ad
Alexander Petrov <alexander.petrov@nav.no>
2022-03-14 09:36:46 +0100

    add logging to interceptor

--- sendsoknad-auth/src/main/java/no/nav/sbl/dialogarena/common/cxf/AzureAdProxyAuthorizationHeaderSetterOutInterceptor.java
+++ sendsoknad-auth/src/main/java/no/nav/sbl/dialogarena/common/cxf/AzureAdProxyAuthorizationHeaderSetterOutInterceptor.java
@@ -5,13 +5,18 @@ import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
+import org.slf4j.Logger;
 import org.springframework.http.HttpHeaders;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 import java.util.List;
 import java.util.Map;
 
 public class AzureAdProxyAuthorizationHeaderSetterOutInterceptor extends AbstractPhaseInterceptor<Message> {
     private final AzureAdTokenService azureAdTokenService;
+    
+    private static final Logger logger = getLogger(AzureAdProxyAuthorizationHeaderSetterOutInterceptor.class);
 
     public AzureAdProxyAuthorizationHeaderSetterOutInterceptor(AzureAdTokenService azureAdTokenService) {
         super(Phase.POST_LOGICAL);
@@ -21,6 +26,7 @@ public class AzureAdProxyAuthorizationHeaderSetterOutInterceptor extends Abstrac
     @Override
     public void handleMessage(Message message) throws Fault {
         var headers = (Map<String, List>) message.get(Message.PROTOCOL_HEADERS);
+        logger.debug("Setting  PROXY_AUTHORIZATION header: " + azureAdTokenService.getToken());
         headers.put(HttpHeaders.PROXY_AUTHORIZATION, List.of("Bearer " + azureAdTokenService.getToken()));
     }
 }
