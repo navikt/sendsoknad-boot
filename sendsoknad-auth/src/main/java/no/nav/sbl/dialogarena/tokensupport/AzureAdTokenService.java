183db5b4ae653b6e9207a159f8290bb7c2c9e169
Eirik <eirikmyklebost+github@gmail.com>
2022-03-14 14:43:20 +0100

    Lagt til HttpRequestHeaderSetterOutInterceptor i PersonInfo service

--- sendsoknad-auth/src/main/java/no/nav/sbl/dialogarena/tokensupport/AzureAdTokenService.java
+++ sendsoknad-auth/src/main/java/no/nav/sbl/dialogarena/tokensupport/AzureAdTokenService.java
@@ -1,7 +1,13 @@
 package no.nav.sbl.dialogarena.tokensupport;
 
+import no.nav.modig.common.SpringContextAccessor;
 import no.nav.security.token.support.client.core.ClientProperties;
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class AzureAdTokenService {
     private final ClientProperties clientProperties;
@@ -15,5 +21,16 @@ public class AzureAdTokenService {
     public String getToken() {
         var response = oAuth2AccessTokenService.getAccessToken(clientProperties);
         return response.getAccessToken();
+    }
+
+    public static Supplier<Map<String, List<String>>> proxyHeaderSupplier() {
+        return () -> {
+            // Dersom det ikke er en Spring-Context, f.eks ved tester, skal ikke interceptoren settes.
+            if (SpringContextAccessor.hasContext()) {
+                var azureAdTokenService = SpringContextAccessor.getBean(AzureAdTokenService.class);
+                return Map.of("x-fss-proxy-authorization", List.of("Bearer " + azureAdTokenService.getToken()));
+            }
+            return Collections.emptyMap();
+        };
     }
 }
