c2b9e8a8ef933618b35c88115f10fd6dfcb3c0c7
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-03-16 15:24:07 +0100

    Removing unused methods

--- sendsoknad-business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/db/soknad/HendelseRepository.java
+++ sendsoknad-business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/db/soknad/HendelseRepository.java
@@ -3,19 +3,14 @@ package no.nav.sbl.dialogarena.soknadinnsending.business.db.soknad;
 import no.nav.sbl.dialogarena.sendsoknad.domain.HendelseType;
 import no.nav.sbl.dialogarena.sendsoknad.domain.WebSoknad;
 
-import java.util.List;
-
 public interface HendelseRepository {
 
     void registrerOpprettetHendelse(WebSoknad soknad);
 
+    // Only used by MigrasjonHandterer
     void registrerMigrertHendelse(WebSoknad soknad);
-
-    void registrerAutomatiskAvsluttetHendelse(String behandlingsId);
 
     void registrerHendelse(WebSoknad soknad, HendelseType hendelse);
 
     Integer hentVersjon(String behandlingsId);
-
-    List<String> hentSoknaderUnderArbeidEldreEnn(int antallDager);
 }
