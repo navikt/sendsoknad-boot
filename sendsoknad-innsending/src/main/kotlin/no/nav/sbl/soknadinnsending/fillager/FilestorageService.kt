14dde014d8bdcefc31c5cf6dc563b01e8a399ec4
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-09-20 18:02:35 +0200

    Updating API with dryRun flag

--- sendsoknad-innsending/src/main/kotlin/no/nav/sbl/soknadinnsending/fillager/FilestorageService.kt
+++ sendsoknad-innsending/src/main/kotlin/no/nav/sbl/soknadinnsending/fillager/FilestorageService.kt
@@ -1,39 +1,32 @@
 package no.nav.sbl.soknadinnsending.fillager
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import no.nav.sbl.soknadinnsending.fillager.dto.FilElementDto
 import no.nav.soknad.arkivering.soknadsfillager.api.FilesApi
-import no.nav.soknad.arkivering.soknadsfillager.infrastructure.ApiClient
-import no.nav.soknad.arkivering.soknadsfillager.infrastructure.Serializer
 import no.nav.soknad.arkivering.soknadsfillager.model.FileData
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class FilestorageService(
-	private val filesClient : FilesApi
-) : Filestorage {
+class FilestorageService(private val filesClient: FilesApi) : Filestorage {
 	private val logger = LoggerFactory.getLogger(javaClass)
-
 
 
 	override fun store(innsendingId: String, files: List<FilElementDto>) {
 		logger.info("$innsendingId: Storing the following files in Soknadsfillager: ${files.map { it.id }}")
 		val filedata = files.map { FileData(it.id, it.content, it.createdAt) }
 
-		filesClient.addFiles(filedata , innsendingId)
+		filesClient.addFiles(filedata, innsendingId, "enabled")
 	}
 
-  override fun getFileMetadata(innsendingId: String, ids: List<String>) :List<FileData> {
+	override fun getFileMetadata(innsendingId: String, ids: List<String>): List<FileData> {
 		logger.info("$innsendingId: Getting metadata for the following files from Soknadsfillager: $ids")
-		return filesClient.findFilesByIds(ids,true,innsendingId)
+		return filesClient.findFilesByIds(ids, true, innsendingId)
 	}
 
 	override fun getFiles(innsendingId: String, ids: List<String>): List<FilElementDto> {
 		logger.info("$innsendingId: Getting the following files from Soknadsfillager: $ids")
 
-		return filesClient.findFilesByIds(ids,false,innsendingId)
+		return filesClient.findFilesByIds(ids, false, innsendingId)
 			.map { FilElementDto(it.id, it.content, it.createdAt) }
 	}
 
@@ -42,6 +35,4 @@ class FilestorageService(
 
 		filesClient.deleteFiles(ids, innsendingId)
 	}
-
-
 }
--- sendsoknad-innsending/src/main/kotlin/no/nav/sbl/soknadinnsending/innsending/InnsendingImpl.kt
+++ sendsoknad-innsending/src/main/kotlin/no/nav/sbl/soknadinnsending/innsending/InnsendingImpl.kt
@@ -1,22 +1,15 @@
 package no.nav.sbl.soknadinnsending.innsending
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import no.nav.sbl.soknadinnsending.innsending.dto.Hovedskjemadata
 import no.nav.sbl.soknadinnsending.innsending.dto.Soknadsdata
 import no.nav.sbl.soknadinnsending.innsending.dto.Vedleggsdata
 import no.nav.soknad.arkivering.soknadsmottaker.api.SoknadApi
-import no.nav.soknad.arkivering.soknadsmottaker.infrastructure.ApiClient
-import no.nav.soknad.arkivering.soknadsmottaker.infrastructure.Serializer
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class InnsendingImpl(
-	val soknadApi: SoknadApi
-) : Innsending {
+class InnsendingImpl(val soknadApi: SoknadApi) : Innsending {
 	private val logger = LoggerFactory.getLogger(javaClass)
-
 
 
 	override fun sendInn(
@@ -27,6 +20,6 @@ class InnsendingImpl(
 		val soknad = createSoknad(soknadsdata, vedleggsdata, hovedskjemas)
 		logger.info("${soknad.innsendingId}: Sending in Soknad to Soknadsmottaker")
 
-		soknadApi.receive(soknad)
+		soknadApi.receive(soknad, "enabled")
 	}
 }
