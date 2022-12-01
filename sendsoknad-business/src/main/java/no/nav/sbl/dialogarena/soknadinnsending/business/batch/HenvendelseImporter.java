6a0b18ec35f3b94e306dfeb0d2659db816260818
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-12-01 19:24:24 +0100

    Do another rerun

--- sendsoknad-business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/HenvendelseImporter.java
+++ sendsoknad-business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/HenvendelseImporter.java
@@ -35,7 +35,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class HenvendelseImporter {
 
     private static final Logger logger = getLogger(HenvendelseImporter.class);
-    private static final String SCHEDULE_TIME = "0 15 17 * * ?"; // At 17:15 every day
+    private static final String SCHEDULE_TIME = "0 30 19 * * ?"; // At 19:30 every day
 
     private final SoknadDataFletter soknadDataFletter;
     private final SoknadRepository lokalDb;
