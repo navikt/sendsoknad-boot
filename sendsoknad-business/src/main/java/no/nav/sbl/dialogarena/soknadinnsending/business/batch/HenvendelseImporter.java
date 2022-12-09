6a4f7fd75048fc518cbccceaff34e064e28eb4e7
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-12-09 16:56:04 +0100

    New migration round

--- sendsoknad-business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/HenvendelseImporter.java
+++ sendsoknad-business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/HenvendelseImporter.java
@@ -32,7 +32,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class HenvendelseImporter {
 
     private static final Logger logger = getLogger(HenvendelseImporter.class);
-    private static final String SCHEDULE_TIME = "0 00 16 * * ?"; // At 16:00 every day
+    private static final String SCHEDULE_TIME = "0 15 17 * * ?"; // At 17:15 every day
     private static final Boolean ER_INNSENDTE_SOKNADER_MED_MANGLENDE_VEDLEGG = true;
 
     private final SoknadDataFletter soknadDataFletter;
