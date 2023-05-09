c9e54a057c399b69bd65a38ada243ca796a2b7d3
nilsarne <nils-arne.ringstad@nav.no>
2023-05-09 08:45:02 +0200

    Lagt til overvåking av arkiveringsstatus, polling av skonadsarkiverers message topic og gjennomgått og endret implementasjon av når vedlegg og annet på søknad skal slettes. Oppdatert flyway versjon

--- sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/FlywayConfig.java
+++ sendsoknad-boot/src/main/java/no/nav/sbl/sendsoknad/FlywayConfig.java
@@ -2,12 +2,14 @@ package no.nav.sbl.sendsoknad;
 
 import no.nav.sbl.dialogarena.soknadinnsending.business.db.DbConfig;
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.ClassicConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import javax.sql.DataSource;
+import java.nio.charset.Charset;
 
 @Configuration
 @Import(DbConfig.class)
@@ -15,15 +17,15 @@ public class FlywayConfig {
 
     @Bean(initMethod = "migrate")
     public Flyway flyway(DataSource datasource) {
-        Flyway flyway = new Flyway();
-        flyway.setBaselineOnMigrate(true);
-        flyway.setDataSource(datasource);
-        flyway.setBaselineOnMigrate(true);
-        flyway.setEncoding("ISO-8859-1");
-        flyway.setBaselineVersionAsString("1.31");
-        flyway.setTable("schema_version");
-        flyway.setValidateOnMigrate(false);
-        flyway.setOutOfOrder(false);
+        ClassicConfiguration config = new ClassicConfiguration();
+        config.setBaselineOnMigrate(true);
+        config.setDataSource(datasource);
+        config.setEncoding(Charset.forName("ISO-8859-1"));
+        config.setBaselineVersionAsString("1.31");
+        config.setTable("schema_version");
+        config.setValidateOnMigrate(false);
+        config.setOutOfOrder(false);
+        Flyway flyway = new Flyway(config);
         return flyway;
     }
 
