dc4be99fe3a75aa8cb576f033af0dbfc3fb3f6b9
nilsarne <nils-arne.ringstad@nav.no>
2023-05-09 10:31:26 +0200

    Lagt til overvåking av arkiveringsstatus, polling av skonadsarkiverers message topic og gjennomgått og endret implementasjon av når vedlegg og annet på søknad skal slettes.

--- sendsoknad-boot/src/main/resources/db/migration/V1_33__legg_til_arkiverinsstatus.sql
+++ sendsoknad-boot/src/main/resources/db/migration/V1_33__legg_til_arkiverinsstatus.sql
@@ -1,1 +1,1 @@
-ALTER TABLE soknad ADD IF NOT EXISTS arkiveringsstatus VARCHAR(20) NOT NULL default 'IkkeSatt';
+ALTER TABLE soknad ADD IF NOT EXISTS arkiveringsstatus VARCHAR(20) default 'IkkeSatt' NOT NULL ;
