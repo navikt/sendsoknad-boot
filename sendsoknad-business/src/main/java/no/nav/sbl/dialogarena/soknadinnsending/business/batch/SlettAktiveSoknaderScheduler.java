99092d6b44247786a7e497db93984549bffb1dcc
nilsarne <nils-arne.ringstad@nav.no>
2024-05-06 09:58:01 +0200

    Oppatering etter PR

--- sendsoknad-business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/SlettAktiveSoknaderScheduler.java
+++ sendsoknad-business/src/main/java/no/nav/sbl/dialogarena/soknadinnsending/business/batch/SlettAktiveSoknaderScheduler.java
@@ -13,14 +13,14 @@ import org.springframework.stereotype.Service;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
-@EnableSchedulerLock(defaultLockAtMostFor = "15m")
+@EnableSchedulerLock(defaultLockAtMostFor = "20m")
 public class SlettAktiveSoknaderScheduler {
 
     private static final Logger logger = getLogger(GamleSoknaderSletterScheduler.class);
 
-    private static final String SCHEDULE_TIME = "0 1 1 JUN * ?"; // at 1 o'clock the 1. of june in the morning
-    private static final int SLETT_GAMLE_SOKNADER_UNDER_ARBEID = 0; // Behold metadata, men slett vedlegg etc. for alle søknader under_arbeid
+    private static final String SCHEDULE_TIME = "0 10 1 JUN * ?"; // at 10 o'clock the 1. of june
 
+    private static final int SLETT_GAMLE_SOKNADER_UNDER_ARBEID = 0; // Behold metadata, men slett vedlegg etc. for alle søknader under_arbeid
 
     private final SoknadRepository soknadRepository;
     private final SoknadService soknadService;
@@ -35,18 +35,14 @@ public class SlettAktiveSoknaderScheduler {
     @Scheduled(cron = SCHEDULE_TIME)
     @SchedulerLock(name = "slettAktiveSoknader", lockAtLeastFor = "15m")
     public void slettGamleSoknader() {
-        if (Boolean.parseBoolean(System.getProperty("sendsoknad.batch.enabled", "true"))) {
-            long startTime = System.currentTimeMillis();
-            logger.info("Starter jobb for å slette alle aaktive søknader");
+        long startTime = System.currentTimeMillis();
+        logger.info("Starter jobb for å slette alle aktive søknader");
 
-            soknadService.automatiskSlettingAvSoknader(HendelseType.AVBRUTT_AUTOMATISK, false, SLETT_GAMLE_SOKNADER_UNDER_ARBEID);
+        soknadService.automatiskSlettingAvSoknader(HendelseType.AVBRUTT_AUTOMATISK, false, SLETT_GAMLE_SOKNADER_UNDER_ARBEID);
 
-            logger.debug(
-                    "Ferdig med å slette vedlegg etc. til ikke innsendte soknader opprettet for mer enn {} dager opprettet for mer enn {} dager. Tidsbruk: {}ms",
-                    SLETT_GAMLE_SOKNADER_UNDER_ARBEID,  (System.currentTimeMillis() - startTime));
-        } else {
-            logger.warn("Batch disabled. Må sette environment property sendsoknad.batch.enabled til true for å slette alle soknader");
-        }
+        logger.debug(
+                "Ferdig med å slette vedlegg etc. til ikke innsendte soknader opprettet for mer enn {} dager opprettet for mer enn {} dager. Tidsbruk: {}ms",
+                SLETT_GAMLE_SOKNADER_UNDER_ARBEID,  (System.currentTimeMillis() - startTime));
     }
 
 }
